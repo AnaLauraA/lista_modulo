95ee4b9952b531f996fd8f40fe5d0d9ceebf72c7
Saulo <sauloandrade0809@gmail.com>
2018-11-25 20:24:50 -0200

    Fixed exercise 15

--- ex15.js
+++ ex15.js
@@ -1,13 +1,12 @@
 function principal(){
-    var vetor =  Number(prompt("Digite os 15 números").split(","));
+    var vetor = (prompt("Digite os 15 números").split(","));
     alert(par(vetor));
 }
-var i = 0;
-var soma = 0;
 function par(vetor){
-
-    for(i;i<3;i++){
-        if (Number(vetor[i])%2 == 0){
+    var i = 0;
+    var soma = 0;
+    for(i;i<15;i++){
+        if (Number(vetor[i]) % 2 == 0){
             soma = Number(vetor[i]) + soma;
             console.log("comehere");
         }
