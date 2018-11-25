ddd4a39513465aa0901aab48655dfcd6348f6536
Saulo <sauloandrade0809@gmail.com>
2018-11-25 19:32:53 -0200

    Fixed exercise 16

--- ex16.js
+++ ex16.js
@@ -1,13 +1,10 @@
 function principal(){
-    var num = document.getElementById("num").value;
-    console.log(num);
+    var num = document.getElementById("num").value.split(",");
     document.getElementById("resposta").innerHTML = somar(num);
 }
-var i = 0;
-var soma = 0; 
 function somar(n){
-    for(i;i<20;i++){
-        console.log(n[i]);
+    var soma = 0; 
+    for(var i = 0;i<20;i++){
         soma = Number(n[i]) + soma;
     }
     return soma;
